b769cbe05735aab6b42632d3bdfa0c091db364c8
Rabonaa <rabemanantsoa.ma@gmail.com>
2026-05-29 14:50:38 +0200

    update

--- Parc_informatique/src/equipements/base/Equipement.java
+++ Parc_informatique/src/equipements/base/Equipement.java
@@ -1,4 +1,59 @@
 package equipements.base;
 
+import java.time.Year;
+
 public class Equipement {
+    private String marque;
+    private String modele;
+    private String numeroSerie;
+    private double prixAchat;
+    private int anneeAchat;
+    private static int anneeCourante = Year.now().getValue();
+
+    public Equipement(String marque, String modele, String numeroSerie, double prixAchat, int anneeAchat){
+        this.marque = marque;
+        this.modele = modele;
+        this.numeroSerie = numeroSerie;
+        this.prixAchat = prixAchat;
+
+        if (anneeAchat >= 2000 && anneeAchat <= anneeCourante)
+            this.anneeAchat = anneeAchat;
+        else this.anneeAchat = anneeCourante;
+    }
+
+    public String getMarque(){
+        return marque;
+    }
+    public String getModele(){
+        return modele;
+    }
+    public String getNumeroSerie(){
+        return numeroSerie;
+    }
+    public double getPrixAchat(){
+        return prixAchat;
+    }
+
+    public int getAnneeAchat(){
+        return anneeAchat;
+    }
+
+    public int calculerAge(){
+        return anneeCourante - anneeAchat;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipement [" +
+                "marque='" + marque + '\'' +
+                ", modele='" + modele + '\'' +
+                ", numeroSerie='" + numeroSerie + '\'' +
+                ", prixAchat=" + prixAchat +
+                ", anneeAchat=" + anneeAchat +
+                ']';
+    }
+
+
+
+
 }
--- Parc_informatique/src/equipements/base/Ordinateur.java
+++ Parc_informatique/src/equipements/base/Ordinateur.java
@@ -1,4 +1,32 @@
 package equipements.base;
 
-public class Ordinateur {
+public class Ordinateur extends Equipement {
+    private String processeur;
+    private int ram;
+    private int stockage;
+    private String typeOS;
+
+    public Ordinateur(String marque, String modele, String numeroSerie, double prixAchat, int anneeAchat,  String processeur, int ram, int stockage, String typeOS){
+        super(marque, modele, numeroSerie, prixAchat, anneeAchat);
+        this.processeur = processeur;
+        this.ram = ram;
+        this.stockage = stockage;
+        this.typeOS = typeOS;
+    }
+    public boolean verifierConfigMinimale(int ramMin, int stockageMin) {
+        return this.ram >= ramMin && this.stockage >= stockageMin;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + " | Ordinateur [" +
+                "processeur='" + processeur + '\'' +
+                ", ram=" + ram + "Go" +
+                ", stockage=" + stockage + "Go" +
+                ", OS='" + typeOS + '\'' +
+                ']';
+    }
+    public String getProcesseur() { return processeur; }
+    public int getRam() { return ram; }
+    public int getStockage() { return stockage; }
+    public String getTypeOS() { return typeOS; }
 }
